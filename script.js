35870b9c6f178facf193b47b9e28e1eaca86ed21
scmirele <mirelecn@gmail.com>
2023-06-19 18:06:16 -0300

    ajustando imagens para o tamanho da div principal

--- script.js
+++ script.js
@@ -2,15 +2,19 @@ let btn = document.querySelector("#next-little-cat-button")
 console.log(btn)
 
 let imgOne = "./assets/cat.two.svg";
-let imgTwo = "./assets/cat.one.svg";
+let imgTwo = "./assets/cat.four.svg";
+let imgThree = "./assets/cat.one.svg";
+let imgFour = "./assets/cat.five.svg";
 
+// colocar em um array
 function mudarImagem() {
     let imagem = document.querySelector('.imagem')
     imagem.src = imgOne;
     let n = imgOne;                    
     imgOne = imgTwo;
-    imgTwo = imgOne;
-    imgTwo = n;
+    imgTwo = imgThree;
+    imgThree = imgFour;
+    imgFour = n;
 }
 
 btn.addEventListener("click", mudarImagem)
